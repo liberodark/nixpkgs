11dd9c94e0430743fa4ecdab55f8c9f8cceef163
liberodark <liberodark@gmail.com>
2025-01-28 19:24:58 +0100

    vcv-rack: 2.5.1 -> 2.6.0

--- pkgs/by-name/vc/vcv-rack/package.nix
+++ pkgs/by-name/vc/vcv-rack/package.nix
@@ -120,7 +120,7 @@ let
 in
 stdenv.mkDerivation rec {
   pname = "vcv-rack";
-  version = "2.5.1";
+  version = "2.6.0";
 
   desktopItems = [
     (makeDesktopItem {
@@ -144,7 +144,7 @@ stdenv.mkDerivation rec {
     owner = "VCVRack";
     repo = "Rack";
     rev = "v${version}";
-    sha256 = "1q2bwjfn6crk9lyd6m3py0v754arw1xgpv5kkj6ka1bc2yz839qh";
+    sha256 = "sha256-leI0wwhYiA8qktJFe6DuZjs6q5tMFQ4WFLD4Ivom5+E=";
   };
 
   patches = [
