571df0be2caa5c3bc7cee72d84eb9c8554222e72
liberodark <liberodark@gmail.com>
2025-02-14 00:15:24 +0100

    openrgb-plugin-effects: 0.9 -> 0.9-unstable-2025-02-13

--- pkgs/by-name/op/openrgb-plugin-effects/package.nix
+++ pkgs/by-name/op/openrgb-plugin-effects/package.nix
@@ -11,13 +11,13 @@
 
 stdenv.mkDerivation (finalAttrs: {
   pname = "openrgb-plugin-effects";
-  version = "0.9";
+  version = "0.9-unstable-2025-02-13";
 
   src = fetchFromGitLab {
     owner = "OpenRGBDevelopers";
     repo = "OpenRGBEffectsPlugin";
-    rev = "release_${finalAttrs.version}";
-    hash = "sha256-8BnHifcFf7ESJgJi/q3ca38zuIVa++BoGlkWxj7gpog=";
+    rev = "713026e96edca2de918f5acf342e92b76ef8584f";
+    hash = "sha256-3WGJMXJExU1H8QiZpAA5mqyGZ63Htx5ZIYeI8waSdys=";
     fetchSubmodules = true;
   };
 
